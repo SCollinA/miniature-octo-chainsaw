83428e7eb0500245d61b0685b094e6cc4cf70a83
Collin Argo <collinargo@gmail.com>
2019-05-20 15:54:37 -0400

    fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-require('dotenv').config()
-import express from 'express';
-import mongoose from 'mongoose';
-import session from 'express-session';
-import bodyParser = require('body-parser');
-const MongoDBStore = require('connect-mongodb-session')(session)
+require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import session from "express-session";
+import bodyParser = require("body-parser");
+const MongoDBStore = require("connect-mongodb-session")(session);
 mongoose.connect(
-    'mongodb://localhost:27017/ts-express-demo',
+    "mongodb://localhost:27017/ts-express-demo",
     {
         useNewUrlParser: true,
         useFindAndModify: false,
@@ -13,13 +13,13 @@ mongoose.connect(
     }
 );
 export const store = new MongoDBStore({
-    uri: 'mongodb://localhost:27017/ts-express-demo',
-    collection: 'sessions'
+    uri: "mongodb://localhost:27017/ts-express-demo",
+    collection: "sessions"
 });
 
-export const app = express();  
-app.use(session({ 
-    secret: 'superSecret',
+export const app = express();
+app.use(session({
+    secret: "superSecret",
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
     },
@@ -28,12 +28,12 @@ app.use(session({
     saveUninitialized: true,
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('build'))  
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.static("build"));
+app.use(bodyParser.json({ limit: "50mb" }));
 
-app.get('/', (req, res) => {
-    res.send('hello world!')
-}) 
+app.get("/", (req, res) => {
+    res.send("hello world!");
+}); 
 
-app.listen(process.env.PORT, () => 
-console.log(`TypeScript-Express app listening on port ${process.env.PORT}!!`))
+app.listen(process.env.PORT, () =>
+console.log(`TypeScript-Express app listening on port ${process.env.PORT}!!`));
